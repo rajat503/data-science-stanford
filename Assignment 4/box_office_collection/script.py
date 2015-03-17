e046704803f727a2ae3fe00ba04b09720e8b62ef
Rajat Agarwal <rajat503@gmail.com>
2015-03-17 21:51:05 +0530

    updated box office task

--- Assignment 4/box_office_collection/script.py
+++ Assignment 4/box_office_collection/script.py
@@ -1,3 +1,5 @@
+import sys
+
 numbers=[6,20,33,35,58,73,77,84,94,113,114,123,143,175,176,247,398,426,435,470]
 print('correct_answer	answer_type	asset_type	asset_file	possible_answers	randomize_answers')
 f = open('CollectionData.csv', 'r')
@@ -9,7 +11,14 @@ for line in f:
 	if i==numbers[j] :
 		s = line.split('","')
 		s[2]=s[2].replace('$','').replace('"','').strip('\n\r')
-		print(s[2]+"	float	none	"+s[1]+'	no answers	false')
 		j += 1
+		k=''.join(e for e in s[1] if e.isalnum() or e==' ')
+		k=k.lower()
+		k=k.replace(' ','_')
+		k=k.replace('__','_')
+		print(s[2]+"	float	text	"+k+'	no answers	false')
+		w=open("assets/"+k+".txt", 'w')
+		w.write(s[1])
+		w.close()
 	i+= 1
 		
